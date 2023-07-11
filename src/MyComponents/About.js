fd75910343fc45936e455720ddc695834667f2c8
Subhash Chandra Verma <104697404+subhash202122@users.noreply.github.com>
2023-07-11 19:01:02 +0530

    Update About.js

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -4,7 +4,10 @@ export const About = () => {
     return (
         <div>
             This is an about component 
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum corporis odio, nesciunt sed eveniet, nemo amet, dignissimos magnam itaque ipsam possimus. Inventore vitae, perferendis harum quas cupiditate adipisci repellendus tempore asperiores at consequatur quos?</p>
+            <p>Built with React.js for efficient task management.
+Reusable components enhance user interface and interaction.
+Create, update, and categorize tasks for effective organization.
+</p>
         </div>
     )
 }
